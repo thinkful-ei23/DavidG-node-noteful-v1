0132df5827e2fcf1703284056b9f8e9988150d3f
David Graves <davidgraves831@gmail.com>
2018-07-30 15:35:09 -0500

    finished finding ID

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const app = express();
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const {id} = req.params;
+  let note = data.find(function(item){
+    return item.id === Number(id);
+  });
+  res.json(note);
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
